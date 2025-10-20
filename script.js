6d92beb421f51ed0895828b8172ac0c207b87e01
Git0104 <us.tan0104@icloud.com>
2025-10-20 21:55:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const supabaseUrl = "https://shfrpnybnlvgckvhhdif.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoZnJwbnlibmx2Z2NrdmhoZGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NDYxMTEsImV4cCI6MjA3NjUyMjExMX0.0hZPFvLFdJw3fNhlbudc0tcCajGfVVXBU0RxDW34NA8";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = supabaseJs.createClient(supabaseUrl, supabaseKey);
 
 const chat = document.getElementById("chat");
 const messageInput = document.getElementById("message");
@@ -31,3 +31,4 @@ sendBtn.addEventListener("click", async () => {
 });
 
 fetchMessages();
+
